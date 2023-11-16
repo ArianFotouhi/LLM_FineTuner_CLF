fee8db51c619062c49a20c2b24cd02e81ef3d018
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-11-16 11:43:03 -0500

    v1.8

--- finetuner_clf.py
+++ finetuner_clf.py
@@ -1,6 +1,13 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, GPTNeoForSequenceClassification
 from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+from transformers import AutoModelForSequenceClassification
+from torch.optim import AdamW
+from transformers import get_scheduler
+import torch
+from tqdm.auto import tqdm
+import evaluate
 
 # Load dataset
 dataset = load_dataset("yelp_review_full")
@@ -24,3 +31,54 @@ tokenized_datasets.set_format("torch")
 small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
 small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
 
+
+#Create Data Loader objects
+train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
+eval_dataloader = DataLoader(small_eval_dataset, batch_size=8)
+
+# Initialize the model
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=5)
+
+#Pipeline config     
+optimizer = AdamW(model.parameters(), lr=5e-5)
+
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model.to(device)
+
+
+# Training
+progress_bar = tqdm(range(num_training_steps))
+
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+# Eval
+
+metric = evaluate.load("accuracy")
+model.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
