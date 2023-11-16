ed27d75b222d97343a0c9677bfe159b885fd4280
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-11-16 11:39:39 -0500

    v1.3

--- finetuner_clf.py
+++ finetuner_clf.py
@@ -1,1 +1,19 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, GPTNeoForSequenceClassification
+from transformers import AutoTokenizer
 
+# Load dataset
+dataset = load_dataset("yelp_review_full")
+print('dataset record example:', dataset["train"][1]) 
+
+
+# Create tokenizer 
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+#
