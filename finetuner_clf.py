9bccfad52c5083ed89ddb3d9e21cbe4ba0b055d7
Arian <78182876+ArianFotouhi@users.noreply.github.com>
2023-11-16 11:40:34 -0500

    v1.5

--- finetuner_clf.py
+++ finetuner_clf.py
@@ -16,4 +16,11 @@ def tokenize_function(examples):
 
 tokenized_datasets = dataset.map(tokenize_function, batched=True)
 
-#
+# Data precessing
+tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format("torch")
+
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
